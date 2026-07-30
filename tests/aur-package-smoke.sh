776e59e219c575a834f18cf2694b45d15908ca45
Valhallab SASU <contact@valhallab.com>
2026-07-31 00:44:39 +0200

    build(aur): prepare pre-alpha 5 metadata

--- tests/aur-package-smoke.sh
+++ tests/aur-package-smoke.sh
@@ -13,9 +13,9 @@ test -f "$install_hook"
 
 grep -Fqx '# Maintainer: Valhallab <contact at valhallab dot com>' "$pkgbuild"
 grep -Fqx 'pkgname=overcrow-bin' "$pkgbuild"
-grep -Fqx 'pkgver=0.1.0prealpha4' "$pkgbuild"
+grep -Fqx 'pkgver=0.1.0prealpha5' "$pkgbuild"
 grep -Fqx 'pkgrel=1' "$pkgbuild"
-grep -Fqx '_release=v0.1.0-pre-alpha.4' "$pkgbuild"
+grep -Fqx '_release=v0.1.0-pre-alpha.5' "$pkgbuild"
 grep -Fqx "arch=('x86_64')" "$pkgbuild"
 grep -Fqx "license=('AGPL-3.0-only')" "$pkgbuild"
 grep -Fqx "provides=('overcrow')" "$pkgbuild"
@@ -26,20 +26,21 @@ grep -Fqx 'install=overcrow.install' "$pkgbuild"
 grep -Fqx "url='https://github.com/Valhallab/PlayerVox-OverCrow'" "$pkgbuild"
 grep -Fq "\${url}/releases/download/\${_release}/" "$pkgbuild"
 grep -Fq \
-    'dd4525a88e50b52c8c4e31aab6b7610736ecc70b8cb76ecc70102878161a711b' \
+    'f39133b67356b09a3a439dc50eeedf5b887a9b419f5871986308402a8b85b1a7' \
     "$pkgbuild"
 grep -Fq "bsdtar -xf \"\$srcdir/\$_source\"" "$pkgbuild"
 
 grep -Fqx 'pkgbase = overcrow-bin' "$srcinfo"
-grep -Fq 'pkgver = 0.1.0prealpha4' "$srcinfo"
+grep -Fq 'pkgver = 0.1.0prealpha5' "$srcinfo"
 grep -Fq 'pkgrel = 1' "$srcinfo"
 grep -Fqx '	depends = libayatana-appindicator' "$srcinfo"
 grep -Fqx '	depends = xdg-utils' "$srcinfo"
+grep -Fqx '	optdepends = gnome-shell: GNOME Wayland overlay integration' "$srcinfo"
 grep -Fq \
-    'github.com/Valhallab/PlayerVox-OverCrow/releases/download/v0.1.0-pre-alpha.4/' \
+    'github.com/Valhallab/PlayerVox-OverCrow/releases/download/v0.1.0-pre-alpha.5/' \
     "$srcinfo"
 grep -Fq \
-    'dd4525a88e50b52c8c4e31aab6b7610736ecc70b8cb76ecc70102878161a711b' \
+    'f39133b67356b09a3a439dc50eeedf5b887a9b419f5871986308402a8b85b1a7' \
     "$srcinfo"
 
 cmp "$project_root/packaging/arch/overcrow.install" "$install_hook"
